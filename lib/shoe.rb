58b7b0b8ce9dcf20a02440ef88ad7003b6cdc203
brindab50 <brindab50@gmail.com>
2019-10-10 12:49:35 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,28 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition 
   attr_reader :brand
+
+  BRANDS = ["Uggs", "Rainbow", "Nike", "Yeezy"]
 
   def initialize(brand)
     @brand = brand
+    ## only push brand into array if its not there yet.
+
+    # we're trying to see if we already have that brand in BRANDS.
+
+    # BRANDS.each do |brand_string|
+    #   if brand == brand_string
+    #     return
+    #   end
+    # end
+
+    found_brand = BRANDS.find do |brand_string|
+      brand_string == brand
+    end
+
+    if found_brand == nil
+      BRANDS << brand
+    end
   end
 
   def cobble
@@ -11,4 +30,10 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+
+
+  # def brands=(brands)
+  #   @brands = brands
+  #   BRANDS << brands
+  # end
 end
